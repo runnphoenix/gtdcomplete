35cc64ca3ccbcceeb1f0d8a7da27ef0f1fba6c98
runnphoenix <runnphoenix@gmail.com>
2017-09-15 14:32:23 +0800

    finish all time statistics function, need to add time filters

--- handlers/dayStatistics.py
+++ handlers/dayStatistics.py
@@ -4,23 +4,20 @@ from handler import Handler
 from models import Project
 from models import TimeCategory
 from models import Event
-
 import accessControl
 
 from datetime import datetime, date, time
 from google.appengine.ext import db
 
 class DayStatistics(Handler):
+
     @accessControl.user_logged_in
     def get(self):
         result = {}
         timeCategories = self.user.timeCategories
         for timeCategory in timeCategories:
-            print timeCategory.name
             categoryTime = 0
-            categoryEvents = timeCategory.events
-            for event in categoryEvents:
-                # Add time
-                print event.title
-                print (event.time_exe_end - event.time_exe_start).seconds / 60
+            for event in timeCategory.events:
+                categoryTime = categoryTime + (event.time_exe_end - event.time_exe_start).seconds / 60
+            result[timeCategory.name] = [categoryTime, float(categoryTime)/24/0.6]
         self.render("dayStatistics.html", result=result)
