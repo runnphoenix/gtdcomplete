bb93bd0ca1995f9703e4834b9dbe0804b7ae7a30
Zhang Hanying <runnphoenix@gmail.com>
2017-10-17 11:37:26 +0800

    fix a samll bug.

--- handlers/newEvent.py
+++ handlers/newEvent.py
@@ -75,7 +75,7 @@ class NewEvent(Handler):
             if con.name == contextName:
                 context = con
 
-        errorMessage = self.erMessage(title，exeStartTime, exeEndTime)
+        errorMessage = self.erMessage(title, exeStartTime, exeEndTime)
 
         if errorMessage:
             self.render("newEvent.html",
