20fe6d3cb0d8f9eb57e1c12f63aeefffce065ccc
runnphoenix@gmail.com <runnphoenix@gmail.com>
2017-10-31 09:40:37 +0800

    show finished events according to dates selected

--- handlers/projectPage.py
+++ handlers/projectPage.py
@@ -3,6 +3,7 @@
 from .handler import Handler
 from models import Project
 from . import accessControl
+from datetime import datetime
 
 
 class ProjectPage(Handler):
@@ -25,7 +26,7 @@ class ProjectPage(Handler):
                 event.delete()
             project.delete()
             self.redirect("/projects")
-        else: #Update
+        elif 'Update Name' in self.request.params: #Update
             project_name = self.request.get('project_name')
             project.name = project_name
             project.put()
@@ -35,16 +36,35 @@ class ProjectPage(Handler):
                 project_name=project.name,
                 finished_events=finished_events,
                 unfinished_events=unfinished_events)
+        else: #Look up throught date
+            startDate = datetime.strptime(self.request.get("startDate"),"%Y-%m-%d")
+            endDate = datetime.strptime(self.request.get("endDate"), "%Y-%m-%d")
+            if startDate > endDate:
+                errMessage = "End date MUST be bigger than start date."
+                self.render("projectPage.html",
+                    project_name=project.name,
+                    finished_events=[],
+                    unfinished_events=[],
+                    errMessage=errMessage)
+            else:  # with duration
+                days = (endDate - startDate).days + 1
+                dates = (startDate + timeDelta(i) for i in range(days))
+                (finished_events, unfinished_events) = self.eventsInContainer(project, dates)
+                self.render("projectPage.html",
+                    project_name=project.name,
+                    finished_events=finished_events,
+                    unfinished_events=unfinished_events)
 
-    def eventsInContainer(self, container):
+    def eventsInContainer(self, container, lookupDates=[]):
         finished_events = {}
         unfinished_events = {}
         for event in container.events:
             if event.finished:
-                if not finished_events.get(str(event.time_exe_start.date())):
-                    finished_events[str(event.time_exe_start.date())] = [event]
-                else:
-                    finished_events[str(event.time_exe_start.date())].append(event)
+                if event.time_exe_start.date() in lookupDates:
+                    if not finished_events.get(str(event.time_exe_start.date())):
+                        finished_events[str(event.time_exe_start.date())] = [event]
+                    else:
+                        finished_events[str(event.time_exe_start.date())].append(event)
             else:
                 if not unfinished_events.get(str(event.time_exe_start.date())):
                     unfinished_events[str(event.time_exe_start.date())] = [event]
