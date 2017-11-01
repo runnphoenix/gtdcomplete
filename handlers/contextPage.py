b586cca09e332db2042c67c0d454b1a9819986f4
runnphoenix@gmail.com <runnphoenix@gmail.com>
2017-11-01 17:11:53 +0800

    typo

--- handlers/contextPage.py
+++ handlers/contextPage.py
@@ -61,15 +61,16 @@ class ContextPage(Handler):
                     startDate=datetime.now(pytz.timezone('Asia/Shanghai')),
                     endDate=datetime.now(pytz.timezone('Asia/Shanghai')))
 
-    def eventsInContainer(self, container):
+    def eventsInContainer(self, container, lookupDates=[]):
         finished_events = {}
         unfinished_events = {}
         for event in container.events:
             if event.finished:
-                if not finished_events.get(str(event.time_exe_start.date())):
-                    finished_events[str(event.time_exe_start.date())] = [event]
-                else:
-                    finished_events[str(event.time_exe_start.date())].append(event)
+                if event.time_exe_start.date() in lookupDates:
+                    if not finished_events.get(str(event.time_exe_start.date())):
+                        finished_events[str(event.time_exe_start.date())] = [event]
+                    else:
+                        finished_events[str(event.time_exe_start.date())].append(event)
             else:
                 if not unfinished_events.get(str(event.time_exe_start.date())):
                     unfinished_events[str(event.time_exe_start.date())] = [event]
