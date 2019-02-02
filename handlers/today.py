0cc1816ffa4f402183c6e81878ff41c77f12eeb5
hanying <runnphoenix@gmail.com>
2019-02-02 17:17:51 +0800

    actually I wrote a bug

--- handlers/today.py
+++ handlers/today.py
@@ -22,10 +22,18 @@ class Today(Handler):
                     if event.time_plan_start.date() == today or event.time_exe_end.date() == today:
                         if event.finished == True:
                             timeD = self.times_diff(event.time_exe_end, event.time_exe_start)
-                            events_today['finished'].append([event, timeD])
+                            self.insert_event_by_time(events_today['finished'], event, timeD, 'exe')
                         else:
                             timeD = self.times_diff(event.time_plan_end, event.time_plan_start)
-                            events_today['unfinished'].append([event, timeD])
+                            self.insert_event_by_time(events_today['unfinished'], event, timeD, 'plan')
+                            #if len(events_today['unfinished']) == 0:
+                                #events_today['unfinished'].append([event, timeD])
+                            #3else:
+                                #for i in range(len(events_today['unfinished'])):
+                                    #if events_today['unfinished'][i][0].time_plan_start <= event.time_plan_start:
+                                        #i += 1
+                                    #else:
+                                        #events_today['unfinished'].insert(i, [event, timeD])
         return events_today
 
     def times_diff(self, time1, time2):
@@ -33,6 +41,29 @@ class Today(Handler):
             return
         else:
             return (time1 - time2).seconds / 60
+
+    def insert_event_by_time(self, events, event, duration, phase):
+        if len(events) == 0:
+            events.append([event,duration])
+        else:
+            if phase == 'plan':
+                for i in range(len(events)):
+                    if events[i][0].time_plan_start < event.time_plan_start:
+                        i += 1
+                    else:
+                        print "plan add"
+                        events.insert(i,[event, duration])
+                        break
+            elif phase == 'exe':
+                for i in range(len(events)):
+                    if events[i][0].time_exe_start <= event.time_exe_start:
+                        i += 1
+                    else:
+                        print 'exe add'
+                        events.insert(i, [event, duration])
+                        break
+            else:
+                return
 
     @accessControl.user_logged_in
     def post(self):
