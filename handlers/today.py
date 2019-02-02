b16c726f7901d35625df842f6083a04ca16d0e8f
hanying <runnphoenix@gmail.com>
2019-02-02 11:58:21 +0800

    added today section, basicly

--- handlers/today.py
+++ handlers/today.py
@@ -2,13 +2,29 @@
 
 from . import accessControl
 from .handler import Handler
-
+from datetime import date
 
 class Today(Handler):
 
     @accessControl.user_logged_in
     def get(self):
-        self.render("today.html", projects=self.user.projects)
+        self.render("today.html", events=self.events_today())
+
+    def events_today(self):
+        events_today = {}
+        events_today['finished'] = []
+        events_today['unfinished'] = []
+        today = date.today()
+        for project in self.user.projects:
+            events = project.events
+            if events is not None:
+                for event in events:
+                    if event.time_plan_start.date() == today or event.time_exe_end.date() == today:
+                        if event.finished == True:
+                            events_today['finished'].append(event)
+                        else:
+                            events_today['unfinished'].append(event)
+        return events_today
 
     @accessControl.user_logged_in
     def post(self):
