f8a4fea8aa789fd5076e530bd837698ebcb3af81
hanying <runnphoenix@gmail.com>
2019-02-26 16:06:10 +0800

    fix bug: TimeCategory page doesn't support POST method

--- handlers/timeCategoriesW.py
+++ handlers/timeCategoriesW.py
@@ -24,8 +24,48 @@ class TimeCategoriesW(Handler):
 
     @accessControl.user_logged_in
     @accessControl.timeCategory_exist
-    def post(self):
-        pass
+    def post(self, timeCategory_id, timeCategory):
+        if 'Delete Category' in self.request.params:
+            for event in timeCategory.events:
+                event.delete()
+            timeCategory.delete()
+            self.redirect("/timeCategories")
+        elif "Update" in self.request.params:
+            timeCategory_name = self.request.get('timeCategory_name')
+            timeCategory.name = timeCategory_name
+            timeCategory.put()
+            (finished_events, unfinished_events) = self.eventsInContainer(timeCategory)
+            self.render("timeCategoriesW.html",
+                timeCategories=self.user.timeCategories,
+                timeCategory_name=timeCategory.name,
+                finished_events=finished_events,
+                unfinished_events=unfinished_events,
+                startDate=datetime.now(pytz.timezone('Asia/Shanghai')),
+                endDate=datetime.now(pytz.timezone('Asia/Shanghai')))
+        else: #Look up finished events
+            startDate = datetime.strptime(self.request.get("startDate"),"%Y-%m-%d")
+            endDate = datetime.strptime(self.request.get("endDate"), "%Y-%m-%d")
+            if startDate > endDate:
+                errMessage = "End date MUST be bigger than start date."
+                self.render("timeCategoriesW.html",
+                    timeCategories=self.user.timeCategories,
+                    timeCategory_name=timeCategory.name,
+                    finished_events=[],
+                    unfinished_events=[],
+                    startDate=datetime.now(pytz.timezone('Asia/Shanghai')),
+                    endDate=datetime.now(pytz.timezone('Asia/Shanghai')),
+                    errMessage=errMessage)
+            else:  # with duration
+                days = (endDate - startDate).days + 1
+                dates = [(startDate + timedelta(i)).date() for i in range(days)]
+                (finished_events, unfinished_events) = self.eventsInContainer(timeCategory, dates)
+                self.render("timeCategoriesW.html",
+                    timeCategories=self.user.timeCategories,
+                    timeCategory_name=timeCategory.name,
+                    finished_events=finished_events,
+                    unfinished_events=unfinished_events,
+                    startDate=datetime.now(pytz.timezone('Asia/Shanghai')),
+                    endDate=datetime.now(pytz.timezone('Asia/Shanghai')))
 
     def eventsInContainer(self, container, lookupDates=[]):
         finished_events = {}
